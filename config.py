1117eee3381a2c5ba06ae5ea8292d0bd85f983f4
CRiSTiK24 <32386386+CRiSTiK24@users.noreply.github.com>
2022-06-06 14:40:44 +0200

    Update config.py
    typo

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-API_KEY = "Change this wit your api key" #write api key in string
+API_KEY = "Change this with your api key" #write api key in string
 API_SECRET = "Change this with private api key" #write private api key in string
